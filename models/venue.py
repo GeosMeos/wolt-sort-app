31120ffe4911c2dda2f3259a55dc8073f8479ff9
george.musayev <george.musayev@gmail.com>
2021-09-12 01:01:45 +0300

    models

---
+++ models/venue.py
@@ -0,0 +1,8 @@
+class Venue:
+    def __init__(self, title, address, delivery_price, status, score, price_range):
+        self.title = title
+        self.address = address
+        self.delivery_price = delivery_price
+        self.status = status
+        self.score = score
+        self.price_range = price_range
