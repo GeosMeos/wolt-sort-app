be049fc58d2979ed46f72fa26af301aa4c22a357
george.musayev <george.musayev@gmail.com>
2021-09-28 18:17:40 +0300

    somewhat working form and table with data.

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from api_wrapper import get_venues_by_address
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def hello():
+    return redirect(url_for("form"))
+
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/data", methods=["POST", "GET"])
+def data():
+    if request.method == "GET":
+        return redirect(url_for("form"))
+    if request.method == "POST":
+        address = request.form.get("Address")
+        form_data = get_venues_by_address(address)
+        return render_template("data.html", form_data=form_data, time=current_time)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
